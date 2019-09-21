a08b5f97e6897835fbd07a053c8eec0903f50f73
william0701 <williamxiao@live.cn>
2019-09-21 18:20:18 +0800

    分离input_tab相关的变量和方法
    将input_tab涉及到的变量和方法存入R/input_tabServer.R，将input_tab涉及到的UI存入R/input_tabUI.R

--- apo/www/R/input_tabServer.R
+++ apo/www/R/input_tabServer.R
@@ -33,17 +33,17 @@ addAttribution=function(session)
   session$sendCustomMessage('attribution_list',toJSON(result,auto_unbox = T))
 }
 ## basicObj:保存运算需要的变量
-rna.exp=""
-micro.exp=""
-target=""
-geneinfo=""
-select.gene=""
-## ensemblObj：保存ensembl需要的变量
-ensembl=useMart(biomart='ensembl',dataset = 'hsapiens_gene_ensembl',host='www.ensembl.org',ensemblRedirect=F)
-archieves=listEnsemblArchives()
-specials=listDatasets(ensembl)
-filters=listFilters(ensembl)
-attributions=listAttributes(ensembl)
-currentSpecial="hsapiens_gene_ensembl"
-currentURL="www.ensembl.org"
+# rna.exp=""
+# micro.exp=""
+# target=""
+# geneinfo=""
+# select.gene=""
+# ## ensemblObj：保存ensembl需要的变量
+# ensembl=useMart(biomart='ensembl',dataset = 'hsapiens_gene_ensembl',host='www.ensembl.org',ensemblRedirect=F)
+# archieves=listEnsemblArchives()
+# specials=listDatasets(ensembl)
+# filters=listFilters(ensembl)
+# attributions=listAttributes(ensembl)
+# currentSpecial="hsapiens_gene_ensembl"
+# currentURL="www.ensembl.org"
 #Input Page Action

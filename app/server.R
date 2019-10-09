d4c97ff86ee533fb58b446a36570cad3978ef4d0
william0701 <williamxiao@live.cn>
2019-10-09 22:11:16 +0800

    哈哈
    上传

--- app/server.R
+++ app/server.R
@@ -331,7 +331,6 @@ shinyServer(function(input,output,session) {
     
     if(group=="micro_invalid_name"){
     len_sep<-length(sep)
-    #这需要判断传进来的是字符串还是数字。。和字符串的比较好像不能直接�?= 然后还要统一大小写，全转换为大写toupper(states)
 
     if(len_sep==1){
       myfunc<-function(x){
@@ -385,7 +384,6 @@ shinyServer(function(input,output,session) {
     value=as.numeric(value)
     draw_x<-(max(expressgene_num)+min(expressgene_num))/2  
 
-    #通过quantile函数�?  
     x2<-quantile(expressgene_num,value,type=3) 
     draw_x2<-round(x2,2)
     svg(filename = paste(basepath,"Plot","microSampleFilter.svg",sep = "/"),family = 'serif')
@@ -422,7 +420,7 @@ shinyServer(function(input,output,session) {
     
     else if(group=="ce_invalid_name"){
       len_sep<-length(sep)
-      #这需要判断传进来的是字符串还是数字。。和字符串的比较好像不能直接�?= 然后还要统一大小写，全转换为大写toupper(states)
+
       if(len_sep==1){
         myfunc<-function(x){
           if(is.character(x)){
@@ -474,7 +472,7 @@ shinyServer(function(input,output,session) {
       
       value=as.numeric(value)
       draw_x<-(max(expressgene_num2)+min(expressgene_num2))/2  
-      #通过quantile函数�?  
+
       x2<-quantile(expressgene_num2,value,type=3) 
       draw_x2<-round(x2,2)
       svg(filename = paste(basepath,"Plot","RNASampleFilter.svg",sep = "/"),family = 'serif')
@@ -520,6 +518,7 @@ shinyServer(function(input,output,session) {
     
     if(group=="sample_Group_micro_invalid_name_panel"){
 
+
       #这里需要讨论确定删选比例的意�?.1:样本基因表达数除以总基因数2：学长说的按照分布函�?
       x2<-quantile(expressgene_num,line,type=3) 
       
@@ -534,6 +533,7 @@ shinyServer(function(input,output,session) {
       # which(expressgene_num>=x2)
       # after_slice_micro.exp<-sect_output_micro.exp[,which(expressgene_num>=x2)]
       
+
 
     }
     else{

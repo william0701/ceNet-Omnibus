4256e703e6dd6534d7b6b99ebb13db4be90975da
william0701 <williamxiao@live.cn>
2020-03-14 22:18:43 +0800

    Update dependency.R

--- dependency.R
+++ dependency.R
@@ -1,11 +1,11 @@
 dependency=data.frame(package=c('parallel','biomaRt','shiny','plyr','ggplot2','jsonlite','shinydashboard','shinyWidgets','DT','ggthemr','tibble','igraph','scales','rhandsontable','PerformanceAnalytics','linkcomm','MCL','visNetwork','colourpicker','ggplotify','survival','survminer','ComplexHeatmap','circlize','formattable','infotheo','ProNet','gprofiler2'),repo=c('CRAN','Bioc','CRAN','CRAN','CRAN','CRAN','CRAN','CRAN','CRAN','github','CRAN','CRAN','CRAN','CRAN','CRAN','CRAN','CRAN','CRAN','CRAN','CRAN','CRAN','CRAN','Bioc','CRAN','CRAN','CRAN','self','CRAN'),stringsAsFactors = F)
 rownames(dependency)=dependency$package
 installed=installed.packages()
-if(!"devtools" %in% dependency)
+if(!"devtools" %in% installed)
 {
   install.packages('devtools')
 }
-if(!"BiocManager" %in% dependency)
+if(!"BiocManager" %in% installed)
 {
   install.packages('BiocManager')
 }
@@ -20,7 +20,7 @@ if(length(Bioc)>0)
 {
   devtools::install_bioc(Bioc,upgrade = 'never')
 }
-if("!ggthemr" %in% install$package)
+if("ggthemr" %in% install$package)
   devtools::install_github("cttobin/ggthemr",upgrade='never')
-if(!"ProNet" %in% install$package)
+if("ProNet" %in% install$package)
   install.packages("https://cran.r-project.org/src/contrib/Archive/ProNet/ProNet_1.0.0.tar.gz",repos = NULL,type = "source")

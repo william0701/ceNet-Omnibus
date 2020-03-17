fb57ad0ad1b6c2f8c4eebc0bf14f6519a2cc028a
william0701 <williamxiao@live.cn>
2020-03-17 17:31:25 +0800

    Update process.js

--- app/www/js/process.js
+++ app/www/js/process.js
@@ -1,35 +1,6 @@
 var biotype_group=new Map();
 var remain=[];
 $(document).ready(function(){
-/*  var speed = 100;
-  var scrollTop = null;
-  var hold = 0;
-  var float_banner;
-  var pos = null;
-  var timer = null;
-  var moveHeight = null;
-  float_banner = document.getElementById("float_banner");
-  $("#float_banner").css({"position":"absolute","left":"50%","top":"50px","width":"1200px","margin-left":"-300px","height":"30px","font-weight":"bold","z-index":"100"})
-  window.onscroll=scroll_ad;
-  function scroll_ad(){
-    scrollTop = document.documentElement.scrollTop+document.body.scrollTop;
-    pos = scrollTop - float_banner.offsetTop;
-    pos = pos/10
-    moveHeight = pos>0?Math.ceil(pos):Math.floor(pos);
-    //alert(moveHeight)
-    if(moveHeight!==0){
-      float_banner.style.top = float_banner.offsetTop+moveHeight+"px";
-      setTimeout(scroll_ad,speed);
-    }
-  }*/
-  
-
-/*  function fnGetTable(oEl) {
-    
-}*/
-  
-  
-  
   creat_geneFilter("MicroRNA filter","microFilterPlot","micro");
   $("a[href='#shiny-tab-process']").on("click",function(e){
     Shiny.setInputValue("interclick",Math.random());

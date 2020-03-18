361ba451b3e921a3040bff47aa9220b279fa617a
william0701 <williamxiao@live.cn>
2020-03-18 20:48:03 +0800

    Update CMI.R

--- app/www/Program/CMI.R
+++ app/www/Program/CMI.R
@@ -2,7 +2,14 @@ CMI=function(g2,g1,param)
 {
   library(infotheo)
   micro=colnames(target)[which(target[g1,]&target[g2,]==T)]
-  S=colSums(micro.exp[micro,])
+  if(length(microR)==1)
+  {
+    S=micro.exp[micro,]
+  }
+  else
+  {
+    S=colSums(micro.exp[micro,])
+  }
   S=discretize(X = S,disc = param$disc,nbins = param$nbin)
   return(condinformation(X = rna.exp[g1,],Y = rna.exp[g2,],S = S ,method = param$est))
 }

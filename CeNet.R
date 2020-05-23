7f42a9dfaa4e3437247300d24215af387b5b10a8
yichen <183960685@qq.com>
2020-05-23 16:14:02 +0800

    Update CeNet.R

--- CeNet.R
+++ CeNet.R
@@ -56,4 +56,3 @@ CeNetOmnibus <- function(maxRequestSize=1000,workpath=tempdir(),projectName=NULL
   #suppressMessages(shiny::runApp(system.file("app", package = "CeNetOmnibus"),launch.browser=TRUE,...))
   shiny::runApp("D:\\ceNet-Omnibus1\\app",launch.browser=TRUE,...)
 }
-
